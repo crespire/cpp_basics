3a545f9c4884986251136b366c229988da0c3414
crespire <li.simmon@gmail.com>
2022-10-25 17:24:04 -0400

    docs: add some notes to vector

--- vectors.cpp
+++ vectors.cpp
@@ -13,3 +13,12 @@ int main() {
   }
   std::cout << '\n';
 }
+
+/*
+  Capacity: number of elements allocated in memory
+  Size: current number of elements in vector
+
+  vector<int> Array; - empty vector
+  Array.reserve(5); - Reserves memory. but size is zero.
+  Array.push_back(value); - No reallocation required, size is now 1.
+*/
